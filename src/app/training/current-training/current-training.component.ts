31f4f1c75ecd3fe547502c84acce5399b8c4269d
Antonis Zalonis <antonis.zalonis@gmail.com>
2023-09-28 15:55:41 +0300

    Added mat-spinner into current training component

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-current-training',
   templateUrl: './current-training.component.html',
   styleUrls: ['./current-training.component.scss']
 })
-export class CurrentTrainingComponent {
+export class CurrentTrainingComponent implements OnInit {
 
+  @Output() trainingStop = new EventEmitter<void>();
+
+  progress = 0;
+  timer: any;
+
+  constructor() {}
+
+  ngOnInit(): void {
+      this.timer = setInterval(() => {
+        this.progress += 5;
+        if(this.progress >= 100){
+          clearInterval(this.timer);
+        }
+      }, 1000);
+  }
+
+  onStopTraining(){
+    clearInterval(this.timer);
+    this.trainingStop.emit();
+  }
 }
--- src/app/training/training.component.ts
+++ src/app/training/training.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-training',
   templateUrl: './training.component.html',
   styleUrls: ['./training.component.scss']
 })
-export class TrainingComponent {
+export class TrainingComponent implements OnInit {
 
+    onGoingTraining = false;
+    constructor() {}
+
+    ngOnInit(): void {
+        
+    }
+    
 }
